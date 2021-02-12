55ca4381db6b735034a96ec533ef162ddb429b9d
Rafi6677 <rafalgiemza6677@gmail.com>
2021-02-12 20:06:34 +0100

    Project setup

---
+++ app/src/main/java/com/example/shoppinglists/presentation/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.shoppinglists.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shoppinglists.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
